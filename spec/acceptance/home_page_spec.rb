d786d743ec2d1d3e252ffdce316de623ccb16b68
Mike Bain <mike@thealphatester.com>
2013-01-28 09:40:19 +1100

    add an assert to the test

--- spec/acceptance/home_page_spec.rb
+++ spec/acceptance/home_page_spec.rb
@@ -4,5 +4,8 @@ require 'acceptance_helper'
 describe 'the home page', :type => :feature do
   it 'will go to the home page' do
     visit '/'
+    within 'dl.information' do
+      page.should have_css('dd.test-me', :text => 'Hello!')
+    end
   end
 end
