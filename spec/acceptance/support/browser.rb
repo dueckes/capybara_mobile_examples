7b16948f7130b3b4d3059e8282eefe2cf542e7ff
Mike Bain <mike@thealphatester.com>
2013-01-28 13:30:34 +1100

    Get chrome and firefox emulating an iphone.

--- spec/acceptance/support/browser.rb
+++ spec/acceptance/support/browser.rb
@@ -1,4 +1,13 @@
+require 'selenium-webdriver'
 class Browser
+  IPHONE_USER_AGENT = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7'
+
+  ScreenSize = Struct.new(:width, :height)
+
+  def initialize
+    @iphone_screen = ScreenSize.new(320, 480)
+  end
+
   def name
     get_browser_from_env.to_sym
   end
@@ -11,6 +20,19 @@ class Browser
       Capybara.app_host = 'http://10.0.2.2'
       Capybara.always_include_port = true
       Capybara::Selenium::Driver.new(app, :browser => :android, :url => 'http://localhost:8080/wd/hub')
+    when :ff_iphone
+      profile = Selenium::WebDriver::Firefox::Profile.new
+      profile['general.useragent.override'] = IPHONE_USER_AGENT
+
+      driver = Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
+      driver.browser.manage.window.size = @iphone_screen
+      driver
+    when :chrome_iphone
+      switches = ["--user-agent=#{IPHONE_USER_AGENT}"]
+
+      driver = Capybara::Selenium::Driver.new(app, :browser => :chrome, :switches => switches)
+      driver.browser.manage.window.size = @iphone_screen
+      driver
     else
       Capybara::Selenium::Driver.new(app, :browser => name)
     end
